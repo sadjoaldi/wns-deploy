10866ccdc793d58b7207f1aefd389bb5d6b164ba
sadjoaldi <alsannealdi@hotmail.com>
2025-06-02 11:17:51 +0200

    test

--- client/src/Wilder.tsx
+++ client/src/Wilder.tsx
@@ -1,18 +1,17 @@
-import React from "react";
 import blank_profile from "./blank-profile-picture-female.png";
 import { Card } from "./styles/elements";
 
 type WilderProps = {
-  name: string
-  city: string
-}
+  name: string;
+  city: string;
+};
 
 function Wilder({ city, name }: WilderProps) {
   return (
     <Card>
       <img src={blank_profile} alt={`${name} Profile`} />
       <h3>{name}</h3>
-      <h4>City</h4>
+      <h4>Manchester City</h4>
       <p>{city}</p>
     </Card>
   );
